1eb1f0e17db314a395232cc1542bfe9d9ca8fbb8
Ben Booth <benbooth493@gmail.com>
2021-07-06 11:19:04 +0100

    Better Packer install.

--- dotfiles/nvim/.config/nvim/lua/plugins.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins.lua
@@ -1,7 +1,34 @@
-return require('packer').startup(function()
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+    execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+    execute "packadd packer.nvim"
+end
+
+local packer_ok, packer = pcall(require, "packer")
+if not packer_ok then
+    return
+end
+
+packer.init {
+    compile_path = require("packer.util").join_paths(vim.fn.stdpath "config", "plugin", "packer_compiled.vim"),
+    git = { clone_timeout = 300 },
+    display = {
+        open_fn = function()
+            return require("packer.util").float { border = "single" }
+        end,
+    },
+}
+
+vim.cmd "autocmd BufWritePost plugins.lua PackerCompile"
+
+return require('packer').startup(function(use)
 
     -- Packer can manage itself as an optional plugin
-    use {'wbthomason/packer.nvim', opt = true}
+    use {'wbthomason/packer.nvim'}
 
     -- Color scheme
     use {'kyazdani42/nvim-web-devicons'}
